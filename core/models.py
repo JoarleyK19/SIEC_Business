39fba5c7a9f7d512639da7131df75fc34c6a6952
joarley <joarley.reis@hotmail.com>
2022-11-02 21:09:19 -0300

    mudanca do manage.py

---
+++ core/models.py
@@ -0,0 +1,114 @@
+from django.db import models
+import uuid
+from stdimage import StdImageField
+
+
+def get_file_path(__instance, filename):
+    ext = filename.split('.')[1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
+
+
+class Base(models.Model):
+    criado = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Servico(Base):
+    service = models.CharField('Serviço', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+
+class ServicoAdicionais(Base):
+    serviceAd = models.CharField('Serviço', max_length=100)
+    descricaoAd = models.TextField('Descrição', max_length=200)
+    imagemAd = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+
+    class Meta:
+        verbose_name = 'Serviço Adicional'
+        verbose_name_plural = 'Serviços Adicionais'
+
+    def __str__(self):
+        return self.serviceAd
+
+
+class Cargo(Base):
+    cargo = models.CharField('Cargo', max_length=100)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self):
+        return self.cargo
+
+
+class Funcionario(Base):
+    nome = models.CharField('Nome', max_length=100)
+    cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
+    bio = models.TextField('Bio', max_length=200)
+    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+    linkedin = models.CharField('Linkedin', max_length=100, default='#')
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+
+    def __str__(self):
+        return self.nome
+
+
+class EmpresasParceiras(Base):
+    empresa = models.CharField('Empresa', max_length=100)
+    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 300, 'height': 300, 'crop': True}})
+
+    class Meta:
+        verbose_name = 'Empresa Parceia'
+        verbose_name_plural = 'Empresas Parceiras'
+
+    def __str__(self):
+        return self.empresa
+
+
+class Noticias(Base):
+    manchete = models.CharField('Manchete', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 300, 'height': 300, 'crop': True}})
+    link = models.CharField('Link', max_length=300)
+
+    class Meta:
+        verbose_name = 'Manchete'
+        verbose_name_plural = 'Manchetes'
+
+    def __str__(self):
+        return self.manchete
+
+class Testemunhos(Base):
+    ICONE_CHOICES = (
+        ('fa-comments', 'Messege'),
+    )
+    depoimento = models.TextField('Depoimento', max_length=300)
+    nome = models.CharField('Nome', max_length=100)
+    icone = models.CharField('Icone', max_length=12, choices=ICONE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Depoimento'
+        verbose_name_plural = 'Depoimentos'
+
+    def __str__(self):
+        return self.depoimento
+
+
+        
---
+++ core/views.py
@@ -0,0 +1,63 @@
+from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
+
+from .models import Servico, ServicoAdicionais, Funcionario, EmpresasParceiras, Noticias, Testemunhos
+from .form import ContatoForm
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        contex = super(IndexView, self).get_context_data(**kwargs)
+        contex['manchete'] = Noticias.objects.order_by('manchete').all()
+        return contex
+
+
+class FormsView(FormView):
+    template_name = 'contato.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('contato')
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(FormsView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(FormsView, self).form_invalid(form, *args, **kwargs)
+
+
+class ServiceView(TemplateView):
+    template_name = 'service.html'
+
+    def get_context_data(self, **kwargs):
+        contex = super(ServiceView, self).get_context_data(**kwargs)
+        contex['service'] = Servico.objects.all()
+        contex['serviceAd'] = ServicoAdicionais.objects.all()
+        return contex
+
+
+class SobrenosView(TemplateView):
+    template_name = 'sobrenos.html'
+
+    def get_context_data(self, **kwargs):
+        contex = super(SobrenosView, self).get_context_data(**kwargs)
+        contex['funcionarios'] = Funcionario.objects.order_by('nome').all()
+        contex['depoimento'] = Testemunhos.objects.order_by('depoimento').all()
+        return contex
+
+
+class EmpresasView(TemplateView):
+    template_name = 'empresas.html'
+
+    def get_context_data(self, **kwargs):
+        contex = super(EmpresasView, self).get_context_data(**kwargs)
+        contex['empresas'] = EmpresasParceiras.objects.order_by('?').all()
+        return contex
+
+
+class PoliticasView(TemplateView):
+    template_name = 'politicas.html'
