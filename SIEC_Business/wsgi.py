a48b1d0ce80cdddb91e6e0642360bce88f034284
joarley <joarley.reis@hotmail.com>
2022-11-03 21:47:31 -0300

    wsgi.py

--- SIEC_Business/wsgi.py
+++ SIEC_Business/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SIEC_Business.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SIEC_Business.settings.dev')
 
 application = Cling(MediaCling(get_wsgi_application()))
