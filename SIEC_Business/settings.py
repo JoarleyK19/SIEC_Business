8f0351f1213760373e84583cd1cba7037dbad380
joarley <joarley.reis@hotmail.com>
2022-11-05 22:49:24 -0300

    settings.py

--- SIEC_Business/settings.py
+++ SIEC_Business/settings.py
@@ -151,7 +151,7 @@ EMAIL_USE_TSL = True
 EMAIL_HOST_PASSWORD = 'fusion'
 DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
 """
-LOGOUT_REDIRECT_URL = 'index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
