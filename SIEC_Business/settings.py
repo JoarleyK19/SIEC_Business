0f7ef2a4bc43d0fdccc16185e3d7ee0153187517
joarley <joarley.reis@hotmail.com>
2022-11-25 20:53:50 -0300

    teste de imagem

--- SIEC_Business/settings.py
+++ SIEC_Business/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,10 +94,15 @@ DATABASES = {
     }
 }
 """
-
+"""
 DATABASES = {
     'default': dj_database_url.config()
 }
+"""
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
